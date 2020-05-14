fd1dabe6dda12f248752ac004fb9935f09591c33
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-05-14 23:55:01 +0300

    Initial commit. Added empty project.

---
+++ CefSharpCookies/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CefSharpCookies
+{
+    public class StartUp
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
