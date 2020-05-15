619a0ddf9a244b6f353c242fd31cb6c67e711f21
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-05-15 21:41:00 +0300

    Moved CEF cache folder to app data.

--- CefSharpCookies/Util/CefConfigUtil.cs
+++ CefSharpCookies/Util/CefConfigUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using CefSharp;
 using CefSharp.WinForms;
 using CefSharpCookies.Enums;
@@ -23,7 +24,7 @@ namespace CefSharpCookies.Util
         {
             CefSettings settings = new CefSettings
             {
-                CachePath = Path.Combine(Directory.GetCurrentDirectory(),
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                     configService.GetString(ConfigKey.CacheFolderName)),
                 PersistSessionCookies = configService.GetBool(ConfigKey.StoreCookies),
                 CefCommandLineArgs = { "renderer-process-limit" , "1"}
